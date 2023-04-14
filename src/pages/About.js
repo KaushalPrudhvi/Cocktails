ba6173cc7095e866994ee4805ec5656874e24585
kaushal <kausht14@gmail.com>
2023-04-14 15:47:56 -0500

    add

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda,
+        deleniti nisi. Sunt, repellat iste voluptas blanditiis odio modi,
+        assumenda corrupti, architecto sed neque natus et quidem fuga autem
+        illum distinctio.
+      </p>
+    </section>
+  );
+};
+
+export default About;
